47f66a4262a95c6d3ce4bf2a08200c4255998695
Ciro Mc <156836104+Ciricundio@users.noreply.github.com>
2025-06-09 19:30:55 -0500

    Initial commit

---
+++ view/template/footer.php
@@ -0,0 +1,33 @@
+<footer>
+    <div class="parent">
+        
+        <div class="div1">
+            <div class="logo-footer mb-3">
+                <img src="../../public/img/Icono/Icon_feet.svg" alt="Icono de Msmartbuy">
+                <span class="msb-text">Msmartbuy</span>
+            </div>
+            <div>
+                <p class="description">
+                Somos MSmartBuy, una plataforma líder en compras en línea. 
+                Ofrecemos una amplia variedad de productos y servicios de 
+                alta calidad para satisfacer las necesidades de nuestros 
+                clientes.
+                </p>
+            </div>
+        </div>
+
+        <div class="div2">
+            <div class="link">
+                <a href="#">Contactanos</a>
+                <a href="#">T&C</a>
+                <a href="#">Miembro Smart</a>
+                <a href="#">Ayuda/PQR</a>
+            </div>
+        </div>
+
+        <div class="div3">
+            <p >© 2024 WURHANDS | All rights reserved</p>
+        </div>
+
+    </div>
+</footer>
