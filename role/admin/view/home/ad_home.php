7c691b49d65ab4325d6f4d70ed37a700c8b3e318
ciro mc <156836104+Ciricundio@users.noreply.github.com>
2025-06-24 09:42:32 -0500

    Se le agrega el la configuración del head.

--- role/admin/view/home/ad_home.php
+++ role/admin/view/home/ad_home.php
@@ -1,3 +1,23 @@
-<?php
 
-echo "Estás en la zona del admin, que pro eres 😼";
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Msmartbuy - Admin</title>
+    <link rel="icon" href="../../public/img/Icono/Icon_adm.svg">
+    <link rel="stylesheet" href="..">
+    <!-- Incluir Font Awesome para los iconos -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+    <!--Boostrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=close" />
+
+</head>
+<body>
+    <?php
+    echo "Estás en la zona del admin, que pro eres 😼";
+    ?>
+</body>
+</html>
